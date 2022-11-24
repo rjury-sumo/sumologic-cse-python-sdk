73c36154c7eb5c8fc6d729a5cc3b4952be6fbcc7
Rick Jury <rjury@sumologic.com>
2022-11-25 10:14:48 +1300

    if you get import path errors do this

---
+++ pypath.sh
@@ -0,0 +1,6 @@
+# do this in root of the project and it will solve
+# Traceback (most recent call last):
+#  File "/Users/rjury/Documents/sumo2022/sumologic-cse-python-sdk/./scripts/insights/resolve_insights.py", line 31, in <module>
+#    from sumologiccse.sumologiccse import SumoLogicCSE
+#ModuleNotFoundError: No module named 'sumologiccse'
+ export PYTHONPATH="`pwd`"

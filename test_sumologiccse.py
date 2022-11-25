632218c2ef199b1308f8c162ab7449ed03ac14ce
Rick Jury <rjury@sumologic.com>
2022-11-25 13:35:43 +1300

    super basic integration test

--- test_sumologiccse.py
+++ test_sumologiccse.py
@@ -13,6 +13,8 @@ logger.setLevel('INFO')
 
 from sumologiccse.sumologiccse import SumoLogicCSE
 cse=SumoLogicCSE(endpoint='us2')
+q = '-status:"closed"'
+i = cse.get_insights(q=q)
 
 class Test_Session:
     def test_run(self):
@@ -20,3 +22,13 @@ class Test_Session:
 
     def test_session_has_endpoint(self):
         assert re.match('https://api.+sumologic.com/api/sec',cse.endpoint)
+
+class Test_Insights:
+    def get_insight_returns_insight(self):
+        logger.info(f"get_insight:{i[0]['readableId']}")
+        assert re.match('INSIGHT-[0-9]+',cse.get_insight(i[0]['id'])[0]['readableId'])
+
+    def test_get_insights_returns_insight(self):
+        q = '-status:"closed"'
+        insights = cse.get_insights(q=q)
+        assert len(insights)>0 and re.match('INSIGHT-[0-9]+',insights[0]['readableId'])

a0996a6eac17d75bdf253070e25cdebd0ab8cad1
Rick Jury <rjury@sumologic.com>
2023-03-28 15:39:13 +1300

    args and list endpoint

--- scripts/insights/get_insights.py
+++ scripts/insights/get_insights.py
@@ -1,15 +1,28 @@
+import argparse, sys
 import json
 import sys
 import time
 import logging
 import os
+import time; 
+import math
+from datetime import datetime
+from datetime import timedelta
 
 logger = logging.getLogger()
 logging.basicConfig()
-logger.setLevel('INFO')
+logger.setLevel('DEBUG')
+
+parser=argparse.ArgumentParser(description="Changes the status and resolution state of a list of CSE insights using a search query, and adds a comment to each for audit. To avoid param error use: --query=\'-status:\"closed\"\'")
+parser.add_argument("--accessid", help="access id (default: SUMO_ACCESS_KEY)", default=os.environ.get('SUMO_ACCESS_ID'))
+parser.add_argument("--accesskey", help='access key (default: SUMO_ACCESS_KEY', default=os.environ.get('SUMO_ACCESS_KEY'))
+parser.add_argument("--endpoint", help="specify an endpoint (default: us2)", default='https://api.us2.sumologic.com/api/sec')
+parser.add_argument("--query", help='q param to select insights to close in DSL format. To avoid param error use: --query=\'-status:\"closed\"\' (default: -status:"closed" readableId:"INSIGHT-20505")', default='-status:"closed"')
+parser.add_argument("--limit", help="Max insights to close (default 50)", default=50, type=int)
+args=parser.parse_args()
 
 from sumologiccse.sumologiccse import SumoLogicCSE
-cse=SumoLogicCSE(endpoint='us2')
+cse=SumoLogicCSE(endpoint=args.endpoint)
 
 # examples of DSL - to get more try copying from urldecoded url in insights UI page
 #q = 'readableId:"INSIGHT-20190"'
@@ -17,10 +30,10 @@ cse=SumoLogicCSE(endpoint='us2')
 # everything
 #q = None
 # status is not closed and created before date 
-q = '-status:"closed" created:>2022-11-17T00:00:00+00:00'
 
-insights = cse.get_insights(q=q)
-logger.info('query: + ' + str(q) + ' returned: ' + str(len(insights)) + ' insights.')
+
+insights = cse.query_insights(q=args.query ,limit=args.limit)
+logger.info('query: + ' + str(args.query) + ' returned: ' + str(len(insights)) + ' insights.')
 
 # print('for example:')
 # print (insights[0])
